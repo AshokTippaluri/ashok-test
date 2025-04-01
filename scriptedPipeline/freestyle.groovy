72f2973df939bb93c1c4dbd26aa4ca8395b6910f
atippalu <ashok.tippaluri@dotdashmdp.com>
2025-04-01 09:44:46 +0000

    groovy

---
+++ scriptedPipeline/freestyle.groovy
@@ -0,0 +1,24 @@
+import jenkins.model.*
+import hudson.model.*
+
+// Get Jenkins instance
+def jenkins = Jenkins.instance
+
+// Define job name
+def jobName = 'My_New_Job'
+
+// Check if job already exists
+if (jenkins.getItem(jobName)) {
+    println "Job '${jobName}' already exists!"
+} else {
+    // Create a new Freestyle Job
+    def job = new FreeStyleProject(jenkins, jobName)
+    
+    // Add a simple shell command as a build step
+    job.buildersList.add(new hudson.tasks.Shell("echo 'Hello, Jenkins!'"))
+
+    // Save the job
+    jenkins.add(job, jobName)
+    job.save()
+    println "Job '${jobName}' created successfully!"
+}
